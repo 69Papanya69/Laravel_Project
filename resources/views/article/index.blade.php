9df75bc60abb07bad710c49bb112df9f0cd5fee8
69Papanya69 <ivashevich61@mail.ru>
2024-11-22 11:44:36 +0300

    create comments

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -1,6 +1,12 @@
 @extends('layout')
 
 @section('content')
+@use('App\Models\User', 'User')
+@if (session('status'))
+    <div class="toast-message alert alert-success">
+        {{ session('status') }}
+    </div>
+@endif
 <div class="container mt-4">
     <h2 class="mb-4 text-center">Articles</h2>
     <table class="table table-hover table-bordered ">
@@ -14,14 +20,15 @@
         </thead>
         <tbody>
             @foreach ($articles as $article)
-                <tr>
-                    <th scope="row">{{ $article->date }}</th>
-                    <td>{{ $article->name }}</td>
-                    <td>{{ $article->desc }}</td>
-                    <td>{{ $article->user_id }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+            <tr>
+                <th scope="row">{{ $article->date }}</th>
+                <td><a href="/article/{{ $article->id }}">{{ $article->name }}</a></td>
+                <td>{{ $article->desc }}</td>
+                <td>{{User::findOrFail($article->user_id)->name }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+{{ $articles->links() }}
 </div>
 @endsection
